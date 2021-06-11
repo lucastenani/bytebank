266adbd4281e4dad736e415addc970b9591083b0
Lucas Tenani <lucas.tenani3@gmail.com>
2021-06-10 22:46:26 -0300

    New transference component has been created

---
+++ src/app/new-transference/new-transference.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'new-transference',
+  templateUrl: './new-transference.component.html',
+  styleUrls: ['./new-transference.component.scss']
+})
+
+export class NewTransferenceComponent {
+
+}
