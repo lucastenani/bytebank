9b5217bc711b65df9110bef8a94a7177ec1aca4c
Lucas Tenani <lucas.tenani3@gmail.com>
2021-06-11 02:02:48 -0300

    Using property Bind to pass form data

--- src/app/new-transference/new-transference.component.ts
+++ src/app/new-transference/new-transference.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-transference.component.scss'],
 })
 export class NewTransferenceComponent {
+  value!: number;
+  destiny!: number;
+
   transfer() {
     console.log('Solicitada nova transferência');
+    console.log('Valor: ', this.value);
+    console.log('Destino: ', this.destiny);
   }
 }
