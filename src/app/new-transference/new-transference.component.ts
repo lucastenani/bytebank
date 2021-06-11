900b90072391b179416dd3c11aa287d3a5b6a638
Lucas Tenani <lucas.tenani3@gmail.com>
2021-06-11 01:39:18 -0300

    Creating the new transference form event

--- src/app/new-transference/new-transference.component.ts
+++ src/app/new-transference/new-transference.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-transference',
   templateUrl: './new-transference.component.html',
-  styleUrls: ['./new-transference.component.scss']
+  styleUrls: ['./new-transference.component.scss'],
 })
-
 export class NewTransferenceComponent {
-
+  transfer() {
+    console.log('Solicitada nova transferência');
+  }
 }
